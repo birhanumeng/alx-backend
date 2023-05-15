e32350a4f618464d28bedea1301e1bcd10cdc8eb
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-15 13:08:06 +0000

    hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -55,10 +55,10 @@ class Server:
         """
         total_pages = ceil(len(self.dataset()) / page_size)
         return {
-                page_size: len(self.get_page(page, page_size)),
-                page: page,
-                data: self.get_page(page, page_size),
-                next_page: page + 1 if page < total_pages else None,
-                prev_page: page - 1 if page != 1 else None,
-                total_pages: total_pages
-                }
+            'page_size': len(self.get_page(page, page_size)),
+            'page': page,
+            'data': self.get_page(page, page_size),
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page != 1 else None,
+            'total_pages': total_pages
+        }
