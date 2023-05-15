33652cab6fcc82fc3fd1c5d16d51e90cedac5c80
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-15 06:51:23 +0000

    simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,7 +4,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple(int):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ The function takes two integer arguments as an integers.
         It return a tuple of size two containing a start index and 
         an end index corresponding to the range of indexes to return
