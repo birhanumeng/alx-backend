bfcb0aadcc16bbb731c9daa55987428395fb12ee
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-15 12:54:06 +0000

    hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -50,12 +50,15 @@ class Server:
                 - prev_page: number of the previous page, None if no previous page
                 - total_pages: the total number of pages in the dataset as an integer
         """
-        total_pages = ceil(len(self.dataset()) / page_size)
+        page_data = self.get_page(page, page_size)
+        total_data = len(self.dataset())
+        total_pages = ceil(total_data / page_size)
+
         return {
-                page_size: len(self.get_page(page, page_size)),
-                page: page,
-                data: self.get_page(page, page_size),
-                next_page: page + 1 if page < total_pages else None,
-                prev_page: page - 1 if page != 1 else None,
-                total_pages: total_pages
-                }
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page != 1 else None,
+            'total_pages': total_pages
+        }
