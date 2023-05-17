94fed9b40db25ce0d49e62f35d16bed7719cb139
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-17 13:34:18 +0000

    least recently used cache

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -12,7 +12,6 @@ class LRUCache(BaseCaching):
         """ Initiliaze the FIFOCache object. """
         super().__init__()
         self.keys = []
-        self.num_ref = {}
 
     def put(self, key, item):
         """ Add an item in to the cache. When the length of the
@@ -23,14 +22,10 @@ class LRUCache(BaseCaching):
             self.cache_data[key] = item
             if key not in self.keys:
                 self.keys.append(key)
-                self.num_ref[key] = 1
-            else:
-                self.num_ref[key] = self.num_ref[key] + 1
             if len(self.keys) > BaseCaching.MAX_ITEMS:
-                lru = min(list(self.num_ref.values()))
-                dis = [k for k, v in self.num_ref.items() if v == lru][0]
-                del self.cache_data[dis]
-                print("DISCARD: {}".format(dis))
+                discard_key = self.keys.pop(0)
+                del self.cache_data[discard_key]
+                print("DISCARD: {:s}".format(discard_key))
 
     def get(self, key):
         """ Get an item by key. """
