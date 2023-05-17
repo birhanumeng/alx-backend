e8c80648f6bc2f36a641344126a80de6141a8154
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-17 07:05:52 +0000

    basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Create a caching system module by inherting the BaseCaching class
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Creating caching system class that inherit the BaseCaching
+        class.
+    """
+    def __init__(self):
+        """ Initiliazing the objects. """
+        super(self)
+
+    def put(self, key, item):
+        """ Add an item in the cache. """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key. """
+        if key:
+            return self.cache_data[key]
+        return None
