ae75d7599b7e15452abeff0910d3fc258aa346e2
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-17 13:47:02 +0000

    least recently used cache

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,15 +1,24 @@
-#!/usr/bin/env python
-""" LRUCache caching system. """
+#!/usr/bin/python3
+''' LRU Caching: Create a class LRUCache that inherits from BaseCaching
+                 and is a caching system
+'''
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """ Implementing LRUCache algorithm by adding key value pairs
-        in to a dictionary.
-    """
+    ''' An LRU Cache.
+        Inherits all behaviors from BaseCaching except, upon any attempt to
+        add an entry to the cache when it is at max capacity (as specified by
+        BaseCaching.MAX_ITEMS), it discards the least recently used entry to
+        accommodate for the new one.
+        Attributes:
+          __init__ - method that initializes class instance
+          put - method that adds a key/value pair to cache
+          get - method that retrieves a key/value pair from cache '''
+
     def __init__(self):
-        """ Initiliaze the FIFOCache object. """
+        ''' Initialize class instance. '''
         super().__init__()
         self.keys = []
 
@@ -30,7 +39,9 @@ class LRUCache(BaseCaching):
                 print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        """ Get an item by key. """
-        if key and key in self.cache_data.keys():
+        ''' Return value stored in `key` key of cache.
+            If key is None or does not exist in cache, return None. '''
+        if key is not None and key in self.cache_data:
+            self.keys.append(self.keys.pop(self.keys.index(key)))
             return self.cache_data[key]
         return None
