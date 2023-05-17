dd65fbe712b47c6cea7d936c8e3e347ba3ec6b2d
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-17 13:55:08 +0000

    least recently used cache

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,45 +1,37 @@
-#!/usr/bin/python3
-''' LRU Caching: Create a class LRUCache that inherits from BaseCaching
-                 and is a caching system
-'''
+#!/usr/bin/env python
+""" LRUCache caching system. """
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    ''' An LRU Cache.
-        Inherits all behaviors from BaseCaching except, upon any attempt to
-        add an entry to the cache when it is at max capacity (as specified by
-        BaseCaching.MAX_ITEMS), it discards the least recently used entry to
-        accommodate for the new one.
-        Attributes:
-          __init__ - method that initializes class instance
-          put - method that adds a key/value pair to cache
-          get - method that retrieves a key/value pair from cache '''
-
+    """ Implementing LRUCache algorithm by adding key value pairs
+        in to a dictionary.
+    """
     def __init__(self):
-        ''' Initialize class instance. '''
+        """ Initiliaze the FIFOCache object. """
         super().__init__()
         self.keys = []
 
     def put(self, key, item):
-        """ Add an item in to the cache. When the length of the
-            dictionary is greater than BaseCaching.MAX_ITEMS(4),
-            the least recently used element is discarded.
-        """
-        if key and item:
+        ''' Add key/value pair to cache data.
+            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
+            discard least recently used entry in cache to accommodate new
+            entry. '''
+        if key is not None and item is not None:
             self.cache_data[key] = item
             if key not in self.keys:
                 self.keys.append(key)
             else:
                 self.keys.append(self.keys.pop(self.keys.index(key)))
             if len(self.keys) > BaseCaching.MAX_ITEMS:
-                discard_key = self.keys.pop(0)
-                del self.cache_data[discard_key]
-                print("DISCARD: {:s}".format(discard_key))
+                discard = self.keys.pop(0)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
         """ Get an item by key. """
         if key and key in self.cache_data.keys():
+            self.keys.append(self.keys.pop(self.keys.index(key)))
             return self.cache_data[key]
         return None
