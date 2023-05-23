f8d8416b3861a8a2bed2a3d52cdfa4ece2e3ee83
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-23 11:25:04 +0000

    basic configuration

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 """ Basic Flask app configuration. """
 
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+
+app = Flask(__name__, static_url_path='')
+from os import getenv
 
 
-app = Flask(__name__)
-
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    """ Home route """
+    """ GET /
+        Return: 0-index.html
+    """
     return render_template('0-index.html')
 
+
 if __name__ == "__main__":
-    app.run()
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
