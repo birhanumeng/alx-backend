fb4ddad1f95ca10188b1682bec2132d8197bde5b
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-23 11:43:51 +0000

    babel configuration

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Basic Flask app configuration """
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Configuring babel """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object('1-app.Config')
+
+@app.route('/')
+def index():
+    """ Route to 1-index.html """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
